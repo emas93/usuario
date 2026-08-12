45ada5a0101215d161415f33a1465b4bc57f6c66
emas93 <eduardamss93@gmail.com>
2026-08-12 15:18:01 -0300

    Adicionadas as classes security

---
+++ src/main/java/com/emas93/usuario/infrastructure/entity/Telefone.java
@@ -0,0 +1,26 @@
+package com.emas93.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "telefones")
+public class Telefone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "numeroTelefone",length = 10)
+    private String numeroTelefone;
+    @Column(name= "ddd",length = 3)
+    private String ddd;
+
+
+}
