fa476b4dfde3a00d9694a38321362e72bcfc760c
emas93 <eduardamss93@gmail.com>
2026-08-12 18:04:56 -0300

    Adicionado o método que salva o usuário

---
+++ src/main/java/com/emas93/usuario/controller/dtos/UsuarioDTO.java
@@ -0,0 +1,19 @@
+package com.emas93.usuario.controller.dtos;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UsuarioDTO {
+    private String email;
+    private String senha;
+    private String nome;
+    private List<EnderecoDTO> enderecos;
+    private List<TelefoneDTO> telefones;
+    private Long Id;
+}
---
+++ src/main/java/com/emas93/usuario/infrastructure/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.emas93.usuario.infrastructure.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+
+        super(message);
+    }
+
+    public ResourceNotFoundException (String message,Throwable throwable){
+        super(message,throwable);
+    }
+
+
+}
